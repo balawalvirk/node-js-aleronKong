492ccdb002df29c818653d302e70fabf55d70bce
awaismehr <awaismehr001@gmail.com>
2022-11-25 00:40:28 +0500

    remove procfile and added node engine version

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import helmet from '@fastify/helmet';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter()
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   await app.register(helmet);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
-  await app.listen(port, '0.0.0.0', (err, address) =>
-    console.log(`Server is running on ${address}`)
-  );
+  await app.listen(port, (err, address) => console.log(`Server is running on ${address}`));
 }
 bootstrap();
