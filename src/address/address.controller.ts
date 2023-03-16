656aa4c8252dd7a74eef14a0f1f80e2f86c38df7
awaismehr <awaismehr001@gmail.com>
2023-03-16 19:33:22 +0500

    added findOne address api

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,16 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  Put,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Put, HttpException, HttpStatus } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { GetUser, ParseObjectId } from 'src/helpers';
 import { UserDocument } from 'src/users/users.schema';
@@ -25,27 +13,32 @@ export class AddressController {
   constructor(private readonly addressService: AddressService, private readonly userService: UsersService) {}
 
   @Post('create')
-  async createAddress(@Body() createAddressDto: CreateAddressDto, @GetUser() user: UserDocument) {
+  async create(@Body() createAddressDto: CreateAddressDto, @GetUser() user: UserDocument) {
     return await this.addressService.createRecord({ ...createAddressDto, creator: user._id });
   }
 
   @Get('find-all')
-  async findAllAddress(@GetUser() user: UserDocument) {
+  async findAll(@GetUser() user: UserDocument) {
     return await this.addressService.findAllRecords({ creator: user._id });
   }
 
+  @Get(':id/find-one')
+  async findOne(@GetUser() user: UserDocument, @Param('id', ParseObjectId) id: string) {
+    return await this.addressService.findOneRecord({ _id: id });
+  }
+
   @Delete(':id/delete')
-  async deleteAddress(@Param('id', ParseObjectId) id: string) {
+  async delete(@Param('id', ParseObjectId) id: string) {
     return await this.addressService.deleteSingleRecord({ _id: id });
   }
 
   @Patch(':id/update')
-  async updateAddress(@Param('id', ParseObjectId) id: string, @Body() updateAddressDto: UpdateAddressDto) {
+  async update(@Param('id', ParseObjectId) id: string, @Body() updateAddressDto: UpdateAddressDto) {
     return await this.addressService.findOneRecordAndUpdate({ _id: id }, updateAddressDto);
   }
 
   @Put(':id/default')
-  async defaultAddress(@Param('id', ParseObjectId) id: string, @GetUser() user: UserDocument) {
+  async default(@Param('id', ParseObjectId) id: string, @GetUser() user: UserDocument) {
     const address = await this.addressService.findOneRecord({ _id: id });
     if (!address) throw new HttpException('Address not found', HttpStatus.BAD_REQUEST);
     await this.userService.findOneRecordAndUpdate({ _id: user._id }, { defaultAddress: id });
